6a69fa0bcfdcba5bf306becf2dfc9df5d1d1782b
danswartcpa <136262514+danswartcpa@users.noreply.github.com>
2023-10-10 19:42:47 -0500

    Remove pipes, set dependencies, update documentation

--- R/ds_xaringan_media_calls.R
+++ R/ds_xaringan_media_calls.R
@@ -5,13 +5,19 @@
 #' @return List of all media calls printed to console
 #' @export
 #'
+#' @importFrom stringr str_extract_all
+#' @importFrom rvest read_html
+#' @importFrom xml2 xml_find_all xml_attr
+#'
 #' @examples
 #' ds_xaringan_media_calls("unique-is-more-important.Rmd")
+#'
 ds_xaringan_media_calls <- function(rmd_file) {
 
-  library(stringr) # %>% str_extract_all str_detect
-  library(rvest) # %>% read_html
-  library(xml2) # read_html xml_find_all xml_attr
+
+  # Specify the path to your Rmarkdown file
+
+  rmd_file <- "unique-is-more-important.Rmd"
 
   # Read the content of the Rmarkdown file
 
@@ -19,36 +25,28 @@ ds_xaringan_media_calls <- function(rmd_file) {
 
   # Combine the lines into a single string
 
-  rmd_text <- rmd_content %>%
-    paste(collapse = "\n")
+  rmd_text <- paste(rmd_content, collapse = "\n")
 
   # Extract all links using regular expressions
 
-  all_links <- c(
-    rmd_text %>%
-      str_extract_all("\\bhttps?://\\S+\\b") %>%
-      unlist(),
-    rmd_text %>%
-      str_extract_all('url\\(\\s*[\'"]?([^\\)"\']+)') %>%
-      unlist()
-  )
+  links1 <- stringr::str_extract_all(rmd_text, "\\bhttps?://\\S+\\b")
+  links2 <- stringr::str_extract_all(rmd_text, 'url\\(\\s*[\'"]?([^\\)"\']+)')
+
+  # Combine the links
+  all_links <- c(unlist(links1), unlist(links2))
+
 
   # Parse the Rmarkdown content as HTML
 
-  rmd_html <- rmd_text %>%
-    read_html()
+  rmd_html <- rvest::read_html(rmd_text)
 
   # Extract links from image tags (img src)
 
-  img_links <- rmd_html %>%
-    xml_find_all(".//img") %>%
-    xml_attr("src")
+  img_links <- xml2::xml_attr(xml2::xml_find_all(rmd_html, ".//img"), "src")
 
   # Extract links from anchor tags (a href)
 
-  a_links <- rmd_html %>%
-    xml_find_all(".//a") %>%
-    xml_attr("href")
+  a_links <- xml2::xml_attr(xml2::xml_find_all(rmd_html, ".//a"), "href")
 
   # Combine all links
 
@@ -58,14 +56,10 @@ ds_xaringan_media_calls <- function(rmd_file) {
 
   target_extensions <- c(".gif", ".mov", ".mp4", ".png", ".jpg", ".jpeg", ".html")
 
-  filtered_links <- all_links %>%
-    str_detect(paste0(target_extensions, collapse = "|")) %>%
-    all_links[.]
+  filtered_links <- all_links[stringr::str_detect(all_links, paste0(target_extensions, collapse = "|"))]
 
-  # Return the list of unique links
+  # Print the list of links
 
-  unique_links <- unique(filtered_links)
+  print(unique(filtered_links))
 
-  return(unique_links)
 }
-
